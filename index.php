fbf1eee8d134b2337d9b9c1955212fcadcf9a6ac
Jabir <220501@student.glu.nl>
2024-05-30 14:21:00 +0200

    frontend commit

--- index.php
+++ index.php
@@ -4,19 +4,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cbr-Theorie</title>
-    <link rel="stylesheet" href="./assest/css/style.css">
+    <title>CBR Theorie</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
-    <header>
-<div>asdffs</div>
-    </header>
-
-
-
-    <footer></footer>
-
+    <div id="container">
+        <nav>
+            <div id="left">
+                <div id="table">
+                    <h6>Tafel</h6>
+                    <h3>1</h3>
+                </div>
+                <div id="separator"></div>
+                <div id="dummy">
+                    <h2>Dummy</h2>
+                    <span>8 Theorie</span>
+                </div>
+            </div>
+            <div id="time">
+                <span>Resetende tijd</span>
+                <span>7</span>
+                <span>minuten</span>
+            </div>
+            <div id="info-buttons">
+                <div id="help" class="icons">
+                    <img src="./assets/img/icons/help.png" alt="">
+                    <span>Help</span>
+                </div>
+                <div id="markeren" class="icons">
+                    <img src="./assets/img/icons/flags.png" alt="">
+                    <span>Markeren</span>
+                </div>
+                <div id="overzicht" class="icons">
+                    <img src="./assets/img/icons/blocks.png" alt="">
+                    <span>Overzicht</span>
+                </div>
+            </div>
+        </nav>
+        <main>
+            <h3>Wie mag eerst?</h3>
+            <img src="image.png" alt="Question Image" style="max-width: 100%;">
+        </main>
+        <footer>
+            <div id="prev-button" class="button">Vorige</div>
+            <div id="questions">Vraag 2 van 12</div>
+            <div id="next-button" class="button">Volgende</div>
+        </footer>
+    </div>
 </body>
 
 </html>
