ce23f245faf899a85c195b3a9e8ab921fa2f2a23
Owen <owenhomanlaw@gmail.com>
2024-05-30 13:39:36 +0200

    commit

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cbr-Theorie</title>
+    <link rel="stylesheet" href="./assest/css/style.css">
 </head>
+
 <body>
-    
+    <header>
+<div>asdffs</div>
+    </header>
+
+
+
+    <footer></footer>
+
 </body>
+
 </html>
